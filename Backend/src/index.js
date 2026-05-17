46daea93f180d6d591a4e5b0bd64954159e8f937
bobodan351 <adebolal351@gmail.com>
2026-05-17 11:42:35 +0100

    commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 // Note: REMOVE your old "const app = express();" line if it's still here!
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://first-chat-app-limm.vercel.app/", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
