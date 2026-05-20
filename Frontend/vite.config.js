74447442c86e79f0bf29a881ffe7df39dbc1eeb7
bobodan351 <adebolal351@gmail.com>
2026-05-20 15:55:52 +0100

    feat: add chat icons and update PWA configuration in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       manifest: {
         name: "WeChat",
         short_name: "WeChat",
+          start_url: "/",
         description: 'Instant real-time chat application',
         theme_color: '#ffffff',
         background_color: '#ffffff',
@@ -21,12 +22,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: './assets/chat.png', // Ensure this image is exactly 192x192px
+            src: '/chat.png', // Ensure this image is exactly 192x192px
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: './assets/chat-512.png', // MUST be a different file that is exactly 512x512px
+            src: '/chat-512.png', // MUST be a different file that is exactly 512x512px
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
